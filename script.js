a20e6de1ab8f9bdc7aa4aa8bb9f14fffc3bd82e3
Gabrielcardosoalvaresdeoliveira <g.alvares2005@gmail.com>
2024-11-01 19:40:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const elem = document.getElementById("icon");
+
+function LightMode() {
+  document.body.classList.toggle("light")
+  console.log("Clicado")
+}
+
+function toggleList() {
+  elem.classList.toggle("fa-bars")
+  elem.classList.toggle("fa-x")
+}
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
